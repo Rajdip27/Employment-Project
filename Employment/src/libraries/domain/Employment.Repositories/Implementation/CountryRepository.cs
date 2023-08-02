f0cc27d5f86d17a8facadcb45d91ea46a329ff67
Rajdip27 <Srajdip920@gmail.com>
2023-08-02 15:42:35 +0600

    country repostiroy add

---
+++ Employment/src/libraries/domain/Employment.Repositories/Implementation/CountryRepository.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Employment.DataAccess.Contracts.CommonInterface.BaseInterface;
+using Employment.DataAccess.DatabaseContext;
+using Employment.Model.Entities;
+using Employment.Service.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employment.Repositories.Implementation
+{
+	public class CountryRepository : RepositoryBase<Country, VMCountry, int>
+	{
+		public CountryRepository(EmploymentDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+		{
+		}
+	}
+}
--- Employment/src/libraries/infrastructure/Employment.Core/CQRS/Country/Command/CreateCountryCommand.cs
+++ Employment/src/libraries/infrastructure/Employment.Core/CQRS/Country/Command/CreateCountryCommand.cs
@@ -1,5 +1,15 @@
-﻿namespace Employment.Core.CQRS.Country.Command;
+﻿using Employment.Service.Models.ViewModel;
+using Employment.Sheared.Models;
+using MediatR;
 
-public class CreateCountryCommand
+namespace Employment.Core.CQRS.Country.Command;
+
+public record CreateCountryCommand(VMCountry vmCountry) : IRequest<CommandResult<VMCountry>>;
+public class CreateCountryCommandHandler : IRequestHandler<CreateCountryCommand, CommandResult<VMCountry>
 {
+	
+	public Task<CommandResult<VMCountry>> Handle(CreateCountryCommand request, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
 }
