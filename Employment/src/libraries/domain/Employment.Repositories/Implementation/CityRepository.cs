41559b49acb8d7cf2c8b211cc69895156771d060
Rajdip27 <Srajdip920@gmail.com>
2023-08-03 17:24:54 +0600

    Add City and State

---
+++ Employment/src/libraries/domain/Employment.Repositories/Implementation/CityRepository.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Employment.DataAccess.Contracts.CommonInterface.BaseInterface;
+using Employment.DataAccess.DatabaseContext;
+using Employment.Model.Entities;
+using Employment.Service.Models.ViewModel;
+
+namespace Employment.Repositories.Implementation;
+
+public class CityRepository : RepositoryBase<City, VMCity, int>
+{
+	public CityRepository(EmploymentDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+	{
+	}
+}
---
+++ Employment/src/libraries/domain/Employment.Repositories/Interface/ICityRepository.cs
@@ -0,0 +1,9 @@
+﻿using Employment.DataAccess.Contracts.CommonInterface;
+using Employment.Model.Entities;
+using Employment.Service.Models.ViewModel;
+
+namespace Employment.Repositories.Interface;
+
+public interface ICityRepository:IRepository<City,VMCity,int>
+{
+}
